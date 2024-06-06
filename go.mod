47ff5af61c718cf37cbb4e7f2bb8843784eb0547
Phillarmonic Software <phillarmonicsoftware@outlook.com>
2024-06-06 16:01:50 +0100

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gitrelease
+
+go 1.22
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+// GitRelease: Fetches the latest release version of a GitHub repository
+
+// Release represents the latest release information from GitHub
+type Release struct {
+	TagName string `json:"tag_name"`
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: gitrelease <repository>")
+		os.Exit(1)
+	}
+
+	repo := os.Args[1]
+	url := fmt.Sprintf("https://api.github.com/repos/%s/releases/latest", repo)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Failed to fetch release: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("Failed to fetch release: %s", resp.Status)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to read response body: %v", err)
+	}
+
+	var release Release
+	if err := json.Unmarshal(body, &release); err != nil {
+		log.Fatalf("Failed to unmarshal JSON: %v", err)
+	}
+
+	fmt.Println(release.TagName)
+}
