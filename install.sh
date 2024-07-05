db9b9d0b7407156425cb80999c78e3056cf81e96
Phillarmonic Software <phillarmonicsoftware@outlook.com>
2024-07-05 12:36:12 +0100

    Updated implementation. Added an installation script.

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
 # Check if the user has curl
 if ! [ -x "$(command -v curl)" ]; then
   echo "Error: curl is not installed. Please install curl before running this script."
