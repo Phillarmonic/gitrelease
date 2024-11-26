f184460de0ece04e799f6461064c10e59d582702
Phillarmonkey <phillarmonkey@outlook.com>
2024-11-26 13:26:35 +0000

    Revert "Chore: Save github API requests"
    This reverts commit 19df9b357b9f3e71770b918c7e20e41ebc150b58.

--- install.sh
+++ install.sh
@@ -36,8 +36,11 @@ else
 fi
 
 # Fetch latest tag from GitHub API
-LATEST_TAG=v2.2.1
-
+LATEST_TAG=$(curl -s "https://api.github.com/repos/Phillarmonic/gitrelease/releases/latest" | sed -n 's/.*"tag_name": "\([^"]*\)".*/\1/p')
+if [ -z "$LATEST_TAG" ]; then
+  echo "Error: Unable to fetch the latest release tag. Please check your internet connection and try again."
+  exit 1
+fi
 echo "Latest GitRelease version is $LATEST_TAG. Downloading..."
 
 # Define the URL and the target path
